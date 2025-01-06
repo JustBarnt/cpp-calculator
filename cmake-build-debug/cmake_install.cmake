4c6da35ebc4af9a8766d182ee0789014f913e811
JustBarnt <functnal@gmail.com>
2025-01-06 12:02:05 -0500

    Rebased from remote, fixed merge conflict. Kept remote changes.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/justbarnt/Developer/c_projects/calculator
+# Install script for directory: /Users/justbarnt/Developer/c_projects/calc-2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "/Users/justbarnt/Developer/c_projects/calculator/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/justbarnt/Developer/c_projects/calc-2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
